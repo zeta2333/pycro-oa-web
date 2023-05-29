c7a4d9015f6f5645deb4cc55a5bb489fdb61b9f4
Pycro <pycro6815@163.com>
2023-05-29 18:20:16 +0800

    5.29 commit

---
+++ src/views/about.vue
@@ -0,0 +1,580 @@
+<template>
+  <div class="container">
+    <van-nav-bar title="关于我们" />
+    <div class="detail-wrap">
+      <div class="item">
+        <div>
+          <div id="vsb_content_4">
+            <p
+              style="
+                text-justify: inter-ideograph;
+                text-align: justify;
+                line-height: 150%;
+                text-indent: 13px;
+                margin: 20px 0px 5px;
+                background: white;
+              "
+            >
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >&nbsp;
+                苏州科技大学由原苏州城建环保学院与原苏州铁道师范学院合并组建而成，是一所以工为主、工理文管艺多学科协调发展的全日制普通高校，是教育部本科教学工作水平评估优秀高校。2017年成为住房和城乡建设部与江苏省共建高校，同年获批江苏省博士学位授予立项建设单位。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校拥有石湖、江枫、天平三个校区，主校区石湖校区毗邻石湖水，坐拥上方山，环境优美，风景迤逦。校园占地面积<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2300</span
+                >亩，现有全日制在校本科生<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >20000</span
+                >余人，研究生<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >3200</span
+                >余人，国际硕士研究生<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >100</span
+                >余人。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校拥有<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >17</span
+                >个硕士学位授权一级学科，<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >14</span
+                >个硕士专业学位授权类别。<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2</span
+                >个学科在全国第四轮学科评估中获评<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >B</span
+                >档，“工程学”、“化学”、“材料科学”和“环境生态学”学科跻身<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >ESI</span
+                >前<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1%</span
+                >；<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >6</span
+                >个学科入选省优势学科建设工程项目，<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >6</span
+                >个学科入选“十四五”省重点学科。<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >64</span
+                >个本科专业中，<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >19</span
+                >个专业入选国家一流本科专业建设点；<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >20</span
+                >个专业入选江苏高校品牌专业建设工程项目；<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >9</span
+                >个专业通过高等学校专业评估（认证）、工程教育专业认证、师范类专业第二级认证。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校现有专任教师<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1600</span
+                >余人，具有高级专业技术职务的教师<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >700</span
+                >余人。拥有中国科学院院士、中国工程院院士、欧洲科学院院士等一批名家名师；国家“杰青”获得者、吴玉章人文社会科学奖优秀奖获得者、“长江学者”特聘教授等国家级人才工程入选者<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >50</span
+                >余人；省“<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >333</span
+                >工程”第二层次在内的各类省级人才工程入选者近<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >500</span
+                >人；省“双创团队”在内的省级以上教学科研创新团队<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >11</span
+                >支。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校曾获国家级教学成果奖一等奖<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1</span
+                >项、二等奖<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >3</span
+                >项，获包括省教学成果特等奖、省研究生培养模式改革成果一等奖在内的省级教学成果奖<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >40</span
+                >余项。获中国音乐最高奖“金钟奖”（合唱）金奖、中国民间文艺最高奖“山花奖”。现有国家级大学生文化素质教育基地、国家级高校学生科技创业实习基地、国家级校外实践教育基地各<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1</span
+                >个，江苏省大学生创新创业实践教育中心<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1</span
+                >个。近五年，学生荣获“挑战杯”全国大学生课外学术科技作品竞赛和创业计划大赛特等奖、中国“互联网<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >+</span
+                >”大学生创新创业大赛金奖、美国大学生数学建模竞赛（<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >MCM/ICM</span
+                >）特等奖提名、中国智能制造挑战赛国赛特等奖、全国大学生节能减排社会实践与科技竞赛特等奖在内的省部级以上学科竞赛等奖项<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >6000</span
+                >余项。建校以来，学校已为国家培养了近<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >20</span
+                >万名各类毕业生。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校现有包括国家地方联合工程研究中心、江苏省重点实验室、江苏省高校哲学社会科学重点研究基地等省级以上科研平台<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >34</span
+                >个。近五年，共承担科研项目近<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >4000</span
+                >项，其中国家科技重大专项、国家重点研发计划项目、国家“<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >973</span
+                >”项目、“<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >863</span
+                >”项目、国家自然科学基金（重点）项目、国家社会科学基金（重点）项目等国家级项目近<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >260</span
+                >项；获省部级以上科技奖项<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >40</span
+                >余项；学术论文被<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >SCI</span
+                >等三大检索收录<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >8000</span
+                >余篇，专利<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1100</span
+                >余项。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校先后编制<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >200</span
+                >余个小城镇规划，完成苏浙皖地区<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1000</span
+                >余个乡村规划。参与大量地方环保政策的制定、企业环境污染治理与环境应急预案的编制工作<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >,</span
+                >配合省市<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >263</span
+                >行动计划，参与完成长三角场地的风险评估和修复等项目百余项，与佛山市南海区合作共建苏科大环境研究院<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >,</span
+                >进行跨域技术输出，为珠三角探索行之有效的污染防治长效机制。苏州乡村振兴研究院起草完成我国首个农业农村现代化评价考核指标体系。苏州国家历史文化名城保护研究院全面服务支撑全国唯一的“国家历史文化名城保护示范区”建设，面向世界贡献古城保护的“苏州方案”。城市发展智库入选中国智库索引（<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >CTTI</span
+                >）<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2022</span
+                >年度高校智库百强榜，长三角一体化发展研究基地升级为江苏省决策咨询重点研究基地。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校先后与<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >20</span
+                >多个国家、地区的近<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >80</span
+                >所高校和研究机构建立了校际合作关系。自<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1993</span
+                >年以来每年承办由国家商务部主办的援外人力资源培训班，为<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >120</span
+                >多个“一带一路”沿线国家和发展中国家<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2000</span
+                >余名环保、能源和物流等领域的官员和技术人员开展培训和学历教育。学校<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2015</span
+                >年成为全国首批承担援外硕士学历学位教育项目高校。学校获批<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >4</span
+                >个中外合作办学项目并全部通过教育部评估，其中<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >3</span
+                >个项目获批省高校中外合作办学高水平示范性建设工程项目，项目学生规模达<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >780</span
+                >余人。学校获批省“十四五”高校国际化人才培养品牌专业立项建设重点项目<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >2</span
+                >项，获批省“十四五”首批高校海外高层次人才引进重点平台建设项目<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >1</span
+                >项，入选“江苏<span
+                  style="
+                    font-family: 宋体, SimSun;
+                    color: #000000;
+                    font-size: 24px;
+                  "
+                  >-</span
+                >韩国高校合作联盟”和省“高校国际中文教育工作联盟”。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                >学校先后荣获“江苏省文明校园”“江苏省高等学校和谐校园”“江苏省平安校园建设示范高校”“江苏省高等学校思想政治教育工作先进集体”
+                “江苏省共青团工作先进单位”等荣誉，多次被评为全国大学生社会实践先进单位、江苏省优秀研究生招生单位、江苏省毕业生就业工作先进单位。</span
+              >
+            </p>
+            <p style="text-indent: 43px">
+              <span
+                style="
+                  font-family: 宋体, SimSun;
+                  color: #000000;
+                  font-size: 21px;
+                "
+                ><span
+                  style="font-family: 宋体; color: #000000; font-size: 21px"
+                  >当前，学校正以习近平新时代中国特色社会主义思想为指导，以培养基础扎实、知识面宽的高素质创新性应用型人才为己任，坚持立德树人，注重内涵建设，强化特色发展，提升办学实力，努力将学校建设成为特色鲜明、品质卓越的高水平教学研究型大学。（</span
+                >2023年3月）</span
+              >
+            </p>
+            <p
+              style="
+                text-justify: inter-ideograph;
+                text-align: justify;
+                line-height: 150%;
+                text-indent: 13px;
+                margin: 20px 0px 5px;
+                background: white;
+              "
+            >
+              <span
+                style="font-family: 宋体, SimSun; color: black; font-size: 21px"
+              ></span
+              >&nbsp;
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "process",
+
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 20px;
+  .detail-wrap {
+    .item {
+      color: #838485;
+    }
+  }
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div>账号切换</div>
+    <!-- <button @click="wjl()" type="default" size="mini">王经理</button> -->
+    <button @click="admin()" type="default" size="mini">管理员</button>
+    <button @click="zhangsan()" type="default" size="mini">张三</button>
+    <button @click="lisi()" type="default" size="mini">李四</button>
+    <div>当前token：{{ token }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+
+  data() {
+    return {
+      token: "",
+    };
+  },
+
+  created() {
+    this.token = window.localStorage.getItem("token");
+  },
+
+  methods: {
+    admin() {
+      window.localStorage.setItem("token", "");
+      let token =
+        "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwMzc2NjYz11EqLU4t8kwBikGYeYm5qUAtiSm5mXlKtQBwMnyHQgAAAA._utPtG_e9EJh38iWWFzJEqY7dhkIll_twXrLqWWziW_0XhQV7yX-oCibxV38FjfT8v5NU7f101nEnxwOR5GO-g";
+      window.localStorage.setItem("token", token);
+      this.token = window.localStorage.getItem("token");
+    },
+
+    zhangsan() {
+      window.localStorage.setItem("token", "");
+      let token =
+        "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwMzc2NTC21FEqLU4t8kxRsjKHMPMSc1OBWqoyEvPSixPzlGoB_LLDnUUAAAA.fXIxUz4WZTarrCGNAQYfw0yt8q-0dX5lalmR3-qOPW7BtBZDPrlmILVXYEDNyVIHqTNeNoi4nkugyNgMFfV4qQ";
+      window.localStorage.setItem("token", token);
+      this.token = window.localStorage.getItem("token");
+    },
+    lisi() {
+      window.localStorage.setItem("token", "");
+      let token =
+        "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNLMwMzc2NjE211EqLU4t8kxRsjKBMPMSc1OBWnIyizOVagEjVCwDQQAAAA.a6eWDCZIkKTUCV3UBrFM6VlTRXYbFs77t-GBas_PmBw4OC8LHQABVPigZITpe2FPaUKIE0llsTZZVLLWrhR8mQ";
+      window.localStorage.setItem("token", token);
+      this.token = window.localStorage.getItem("token");
+    },
+
+    zzjl() {
+      window.localStorage.setItem("token", "");
+      let token =
+        "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJScgwN8dANDXYNUtJRSq0oULIyNDM3NjMwNLYw11EqLU4t8kxRsjKFMPMSc1OBWqqqsnKUagGHebUNQQAAAA.FcCkD6QfaP8bd_3MYsRD6Tp9wr9JSRFWIUuIdG5O7XxhIeyWfzGLp40_Lesx_kWMMaaskh_ba3NrTlj8-nP6OA";
+      window.localStorage.setItem("token", token);
+      this.token = window.localStorage.getItem("token");
+    },
+  },
+};
+</script>
